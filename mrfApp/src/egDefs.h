52c7aa2db132a377566e82b6cdf8a5ccb1b699e0
Stephanie Allison <saa@slac.stanford.edu>
2006-11-22 16:59:43 +0000

    Remove EG_SEQ_RAM_EVENT defines - use ones in mrfCommon.h instead. Remove REC_EGEVENT defines - use ones in ereventRecord.h instead.

--- mrfApp/src/egDefs.h
+++ mrfApp/src/egDefs.h
@@ -1,4 +1,4 @@
-/* $Id: egDefs.h,v 1.1.1.1 2006/06/19 13:14:36 saa Exp $ */
+/* $Id: egDefs.h,v 1.2 2006/11/02 23:24:25 saa Exp $ */
 #ifndef EPICS_EGDEFS_H
 #define EPICS_EGDEFS_H
 
@@ -16,23 +16,4 @@ typedef struct
 
 #define EG_SEQ_RAM_SIZE                 (1024*512)
  
-#define EG_SEQ_RAM_EVENT_NULL                   0
-#define EG_SEQ_RAM_EVENT_END                    127
-#define EG_SEQ_RAM_EVENT_FREEZE                 126
-#define EG_SEQ_RAM_EVENT_RESET_TIME             125
-#define EG_SEQ_RAM_EVENT_TICK_TIME              124
-#define EG_SEQ_RAM_EVENT_RESET_PRESCALE         123
-#define EG_SEQ_RAM_EVENT_HEARTBEAT              122
-
-#define REC_EGEVENT_UNIT_TICKS  	(0)
-#define REC_EGEVENT_UNIT_FORTNIGHTS	(1)
-#define REC_EGEVENT_UNIT_WEEKS		(2)
-#define REC_EGEVENT_UNIT_DAYS		(3)
-#define REC_EGEVENT_UNIT_HOURS		(4)
-#define REC_EGEVENT_UNIT_MINUITES	(5)
-#define REC_EGEVENT_UNIT_SECONDS	(6)
-#define REC_EGEVENT_UNIT_MILLISECONDS	(7)
-#define REC_EGEVENT_UNIT_MICROSECONDS	(8)
-#define REC_EGEVENT_UNIT_NANOSECONDS	(9)
-
 #endif
