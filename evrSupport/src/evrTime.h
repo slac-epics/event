065722e44954f38a56c7397323bab1508a4e75d8
Stephanie Allison <saa@slac.stanford.edu>
2007-01-06 23:47:48 +0000

    Added more defines from evrPattern.h. Added PULSEID_MAX_MINUS_2 for use by fiducial pulse ID checking. Change evrTime types to be 0 to 3 instead of 1 to 4 so that TSE=0 uses the current pulse's timestamp. Remove evrTimeGetSystem - no longer used externally.

--- evrSupport/src/evrTime.h
+++ evrSupport/src/evrTime.h
@@ -37,9 +37,23 @@ extern "C" {
 #define PULSEID_BIT17           (0x00020000)    /* Bit up from pulse ID*/
 #define PULSEID_INVALID         (0x0001FFFF)    /* Invalid Pulse ID    */
 #define PULSEID_MAX             (0x0001FFDF)    /* Rollover value      */
+#define PULSEID_MAX_MINUS_2     (0x0001FFDD)    /* Rollover value - 2  */
 #define PULSEID_RESYNC          (0x0001E000)    /* Resync Pulse ID     */
 #define NSEC_PER_SEC            1E9             /* # nsec in one sec   */
 
+/* Masks used to decode beam code and YY from modifier1 */
+#define BEAMCODE_BIT_MASK       (0x0000001F)  /* Beam code mask        */
+                                              /* Left shift 8 first    */
+#define YY_BIT_MASK             (0x000000FF)  /* YY code mask          */
+
+/* Mask used to decode timeslot 1 to 6 from modifier2   */
+#define TIMESLOT_MASK           (0x0000003F)  /* timeslot   mask       */
+#define TIMESLOT1_MASK          (0x00000001)  /* timeslot 1 mask       */
+#define TIMESLOT2_MASK          (0x00000002)  /* timeslot 2 mask       */
+#define TIMESLOT3_MASK          (0x00000004)  /* timeslot 3 mask       */
+#define TIMESLOT4_MASK          (0x00000008)  /* timeslot 4 mask       */
+#define TIMESLOT5_MASK          (0x00000010)  /* timeslot 5 mask       */
+#define TIMESLOT6_MASK          (0x00000020)  /* timeslot 6 mask       */
 
 /* For status */
 typedef enum {
@@ -48,12 +62,11 @@ typedef enum {
 
 /* For time ID */
 typedef enum {
-  evrTimeCurrent=1, evrTimeNext1=2, evrTimeNext2=3, evrTimeNext3=4
+  evrTimeCurrent=0, evrTimeNext1=1, evrTimeNext2=2, evrTimeNext3=3
 } evrTimeId_te;
-#define  MAX_EVR_TIME  evrTimeNext3
+#define  MAX_EVR_TIME  4
 
 int evrTimeGet       (epicsTimeStamp  *epicsTime_ps, evrTimeId_te id);
-int evrTimeGetSystem (epicsTimeStamp  *epicsTime_ps, evrTimeId_te id);
 int evrTimePut       (epicsTimeStamp  *epicsTime_ps, evrTimeStatus_te status);
 int evrTimePutPulseID(epicsTimeStamp  *epicsTime_ps, unsigned int pulseID);
 
