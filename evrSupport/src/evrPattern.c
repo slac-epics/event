547691d23a25d20d36209f8020867b616f939f11
Stephanie Allison <saa@slac.stanford.edu>
2007-01-07 00:03:13 +0000

    Removed evrPatternSimMod5 - it's replaced by evrModifier5. Changed evrPatternInit to evrPatternProcInit.  Waveform is now unsigned long instead of long. Changed evrPatternSimProc to evrPatternSim. Added more counters and evrPatternState to make them available for display and to reset them.  Add counter increments in the appropriate logic. Changed evrPatternSimInit to evrPatternInit. Changed evrPatternSim to create and send an EVR pattern waveform.  It is now a sub instead of sSub record. Change evrPatternWaveform_ts to evrQueuePattern_ts which is now in evrQueue.h. Remove YY processing.  Only slc-aware needs it and its been added to slc-aware databases. Replace the PNET structure with an array.

--- evrSupport/src/evrPattern.c
+++ evrSupport/src/evrPattern.c
@@ -1,6 +1,10 @@
 /*=============================================================================
  
   Name: evrPattern.c
+           evrPatternInit      - General Initialization
+           evrPatternProcInit  - Pattern Setup Initialization
+           evrPatternProc      - 360Hz Pattern Setup
+           evrPatternState     - Pattern Processing State and Diagnostics
 
   Abs: This file contains all subroutine support for evr Pattern processing
        records.
@@ -38,26 +42,44 @@
   int evrSubFlag = DP_DEBUG;
 #endif
 
-
 /* c includes */
 
-#include <subRecord.h>        /* for struct subRecord      */
-#include <sSubRecord.h>       /* for struct ssubRecord     */
-#include <registryFunction.h> /* for epicsExport           */
-#include <epicsExport.h>      /* for epicsRegisterFunction */
-#include <dbAccess.h>         /* dbGetTimeStamp */
-#include <alarm.h>            /* NO_ALARM, INVALID_ALARM */
+#include "subRecord.h"        /* for struct subRecord      */
+#include "registryFunction.h" /* for epicsExport           */
+#include "epicsExport.h"      /* for epicsRegisterFunction */
+#include "dbAccess.h"         /* dbGetPdbAddrFromLink      */
 
-#include "evrTime.h"
-#include "evrPattern.h"       
+#include "evrQueue.h"         /* for EVR_QUEUE_PATTERN*    */
+#include "evrTime.h"          /* evrTime* prototypes       */
+
+void evrSend(void *pCard, epicsInt16 messageSize, void *message);
 
 #define  EVR_OK 0
 #define  EVR_INVALID 1
 
+static unsigned int msgCount         = 0; /* # waveforms processed since boot/reset */ 
+static unsigned int msgRolloverCount = 0; /* # time msgCount reached EVR_MAX_INT    */ 
+static unsigned int patternErrCount  = 0; /* # bad PNET waveforms       */
 
 /*=============================================================================
 
   Name: evrPatternInit
+
+  Abs:  General purpose initialization required since all subroutine records
+   require a non-NULL init routine even if no initialization is required.
+   Note that most subroutines in this file use this routine as an init
+   routine.  If init logic is needed for a specific subroutine, create a
+   new routine for it - don't modify this one.
+  
+==============================================================================*/ 
+static int evrPatternInit(subRecord *psub)
+{
+  return 0;
+}
+
+/*=============================================================================
+
+  Name: evrPatternProcInit
 
   Abs:  Initialization for 360Hz pattern processing.  Initialize pointer
         to waveform data.
@@ -79,20 +101,21 @@
   Ret:  0 = OK, -1 = Error.
   
 ==============================================================================*/ 
-static int evrPatternInit(subRecord *psub)
+static int evrPatternProcInit(subRecord *psub)
 {
   DBADDR      *wfAddr = dbGetPdbAddrFromLink(&psub->inpa);
 
   /*
    * inpa must be a DB link and must be the proper type.
    */
-  if (wfAddr && (wfAddr->field_type == DBF_LONG))
+  if (wfAddr && (wfAddr->field_type == DBF_ULONG))
     psub->dpvt = (void *)wfAddr->pfield;
   else
     psub->dpvt = 0;
   if (psub->dpvt) return 0;
   else            return -1;
 }
+
 /*=============================================================================
 
   Name: evrPatternProc
@@ -100,8 +123,8 @@ static int evrPatternInit(subRecord *psub)
   Abs:  360Hz Processing, Grab 7 pattern longs from PATTERNDATA waveform and
         parse into MODIFIER1-5 longins and two longin evr timestamps.
 
-        Then parse out 2 longins from MODIFIER 1 for BEAMCODE, YY 
-		PULSEID is contained in the lower 17 bits of timestamp nsec integer 
+        Then parse out BEAMCODE from MODIFIER 1 and
+	PULSEID from the lower 17 bits of timestamp nsec integer 
 		
   Args: Type	            Name        Access	   Description
         ------------------- -----------	---------- ----------------------------
@@ -132,7 +155,7 @@ Error writing EVR timestamp set record invalid and pulse ID to 0
 	H - MODIFIER5N-3 (evr)
 	I - BUNCHARGEN-3 (evr)
 	J - BEAMCODEN-3 (decoded from PNET bits 8-12, MOD1 8-12)
-	K - YYN-3       (decoded from PNET bits 0-7, MOD1 0-7)
+	K - Spare
 	L - PULSEIDN-3  (decoded from PNET bits, 17)   
 	VAL = 1 = error; 0 = OK
    Output to evr timestamp table
@@ -143,38 +166,46 @@ static long evrPatternProc(subRecord *psub)
 { 
 
   DBADDR                *wfAddr = dbGetPdbAddrFromLink(&psub->inpa);
-  evrPatternWaveform_ts *evrPatternWF_ps = (evrPatternWaveform_ts *)psub->dpvt;
+  evrQueuePattern_ts    *evrPatternWF_ps = (evrQueuePattern_ts *)psub->dpvt;
   int                    status = 0;
 
+  /* Keep a count of messages and reset before overflow */
+  if (msgCount < EVR_MAX_INT) {
+    msgCount++;
+  } else {
+    msgRolloverCount++;
+    msgCount = 0;
+  }
+  
   /* if waveform is invalid or has invalid size or type            */
   /*   set record invalid; pulse id =0, and                        */
   /*   evr timestamp/status to last good time with invalid status */
   if (psub->b ||
-      (psub->c < (sizeof(evrPatternWaveform_ts)/sizeof(epicsUInt32))) ||
+      (psub->c < (sizeof(evrQueuePattern_ts)/sizeof(epicsUInt32))) ||
       (!psub->dpvt) || (!wfAddr)) {
-    psub->d = psub->e = psub->f = psub->g = psub->h = psub->i = psub->j = psub->k = 0.0;
+    if (psub->b) patternErrCount++;
+    psub->d = psub->e = psub->f = psub->g = psub->h = psub->i = psub->j = 0.0;
     status = -1;
   } else {
 
     dbScanLock(wfAddr->precord);
     /* error if the waveform has an invalid type or version */
-    if ( (evrPatternWF_ps->header_s.type    != EVR_WF_PATTERN) ||
-       (evrPatternWF_ps->header_s.version != EVR_WF_PATTERN_VERSION)) {
-      psub->d = psub->e = psub->f = psub->g = psub->h = psub->i = psub->j = psub->k = 0.0;
+    if ( (evrPatternWF_ps->header_s.type    != EVR_QUEUE_PATTERN  ) ||
+         (evrPatternWF_ps->header_s.version != EVR_QUEUE_PATTERN_VERSION)) {
+      patternErrCount++;
+      psub->d = psub->e = psub->f = psub->g = psub->h = psub->i = psub->j = 0.0;
       status = -1;
     } else {
       /* set outputs to the modifiers */
-      psub->d = (double)(evrPatternWF_ps->pnet_s.modifier1);
-      psub->e = (double)(evrPatternWF_ps->pnet_s.modifier2);
-      psub->f = (double)(evrPatternWF_ps->pnet_s.modifier3);
-      psub->g = (double)(evrPatternWF_ps->pnet_s.modifier4);
+      psub->d = (double)(evrPatternWF_ps->modifier_a[0]);
+      psub->e = (double)(evrPatternWF_ps->modifier_a[1]);
+      psub->f = (double)(evrPatternWF_ps->modifier_a[2]);
+      psub->g = (double)(evrPatternWF_ps->modifier_a[3]);
       psub->h = (double)(evrPatternWF_ps->modifier5);
       psub->i = (double)(evrPatternWF_ps->bunchcharge);
       /* beamcode decoded from modifier 1*/
-      psub->j = (double)( (evrPatternWF_ps->pnet_s.modifier1 >> 8) &
+      psub->j = (double)( (evrPatternWF_ps->modifier_a[0] >> 8) &
                           BEAMCODE_BIT_MASK );
-      /* yy decoded from modifier 1 bits */
-      psub->k = (double)(evrPatternWF_ps->pnet_s.modifier1 & YY_BIT_MASK);
       /* decode pulseid field to output j (keep lower 17 bits) */
       psub->l = (double)(evrPatternWF_ps->time.nsec & LOWER_17_BIT_MASK);
       /* write to evr timestamp table and error check */
@@ -195,181 +226,131 @@ static long evrPatternProc(subRecord *psub)
   return status;
  
 }
+
 /*=============================================================================
 
-  Name: evrPatternSimInit
+  Name: evrPatternState
 
-  Abs:  General purpose initialization required since all subroutine records
-   require a non-NULL init routine even if no initialization is required.
-   Note that most subroutines in this file use this routine as an init
-   routine.  If init logic is needed for a specific subroutine, create a
-   new routine for it - don't modify this one.
+  Abs:  Access to Last Pattern State and Pattern Diagnostics.
+        This subroutine is for status only and should update at a low
+        rate like 1Hz.
+
+  Args: Type                Name        Access     Description
+        ------------------- ----------- ---------- ----------------------------
+        subRecord *         psub        read       point to subroutine record
+
+  Rem:  Subroutine for IOC:LOCA:UNIT:PATTERNDIAG
+
+  Inputs:
+       A - Error Flag from evrPatternProc
+       B - Counter Reset Flag
+       C - Spare
+     
+  Outputs:
+       D - Number of waveforms processed by this subroutine
+       E - Number of times D has rolled over
+       F - Number of bad waveforms
+           The following G-I outputs are pop'ed by a call to proc. evrQueueCounts()
+       G - Number of times ISR tried to send a message
+       H - Number of times G has rolled over
+       I - Number of times ISR failed to send a message
+       VAL = Last Error flag (see evrPatternProc for values)
+
+  Side: File-scope counters may be reset.
   
-==============================================================================*/ 
-static int evrPatternSimInit(sSubRecord *psub)
-{
+  Ret:  0 = OK
 
+==============================================================================*/
+static long evrPatternState(subRecord *psub)
+{
+  psub->val = psub->a;
+  if (psub->b > 0.5) {
+    psub->b = 0.0;
+    msgCount              = 0;
+    msgRolloverCount      = 0;
+    patternErrCount       = 0;
+    evrQueueCountReset(EVR_QUEUE_PATTERN);
+  }
+  psub->d = msgCount;          /* # waveforms processed since boot/reset */
+  psub->e = msgRolloverCount;  /* # time msgCount reached EVR_MAX_INT    */
+  psub->f = patternErrCount;
+  evrQueueCounts (EVR_QUEUE_PATTERN,&psub->g,&psub->h,&psub->i); 
   return 0;
 }
+
 /*=============================================================================
 
-  Name: evrPatternSimProc
+  Name: evrPatternSim
 
-  Abs:  360Hz Processing, Grab 7 pattern longs from PATTERNDATA waveform and
-        parse into MODIFIER1-5 longins and two longin evr timestamps.
-
-        Then parse out 2 longins from MODIFIER 1 for BEAMCODE, YY 
-		PULSEID is contained in the lower 17 bits of timestamp nsec integer 
+  Abs:  Simulate EVR data stream. Used for simulation/debugging only.
 		
   Args: Type	            Name        Access	   Description
         ------------------- -----------	---------- ----------------------------
         subRecord *         psub        read       point to subroutine record
 
-  Rem:  Subroutine for IOC:LOCA:UNIT:PATTERN
+  Rem:  Subroutine for IOC:LOCA:UNIT:PATTERNSIM
 
-  Side: Waveform order:
-        4 PNET 32 bit unsigned integers (MODIFIER 1-4)
-		1 EVR 32 bit unsigned integer (MODIFIER 5)
-		2 EVR timestamp 32 bit unsigned integers 
-		1 bunchcharge 32 bit integer (picoCoulombs)
-  Error checking:
-Waveform record is invalid or has invalid size or type set record invalid, pulse ID to 0, 
-and EVR timestamp/status to last-good-time/invalid
-Error writing EVR timestamp â€“ set record invalid and pulse ID to 0
-
+  Side: Sends a message to the EVR pattern queue.
+  
   Sub Inputs/ Outputs:
-   Inputs:
-    B - 1 = INVALID WF
-        0 = VALID WF
- Now, for the simulated inputs (instead of the waveform:
-    M - MODIFIER1
-    N - MODIFIER2
-    O - MODIFIER3
-    P - MODIFIER4
-    Q - MODIFIER5  
-    U - BUNCHARGE  
-    V - PULSEID
-    W - YY
-    X - BEAMCODE
-   Outputs:
-	D - MODIFIER1N-3 (PNET)
-	E - MODIFIER2N-3 (PNET)
-	F - MODIFIER3N-3 (PNET)
-	G - MODIFIER4N-3 (PNET)
-	H - MODIFIER5N-3 (evr)
-	I - BUNCHARGEN-3 
-	J - BEAMCODEN-3 (decoded from PNET bits 8-12, MOD1 8-12)
-	K - YYN-3       (decoded from PNET bits 0-7, MOD1 0-7)
-	L - PULSEIDN-3  (decoded from PNET bits, 17)
-   
-	VAL = 1 = error; 0 = OK
-   Output to evr timestamp table
+    Simulated inputs:
+    D - MODIFIER1
+    E - MODIFIER2
+    F - MODIFIER3
+    G - MODIFIER4
+    H - MODIFIER5  
+    I - BUNCHARGE  
+    J - BEAMCODE
+    K - YY
   Ret:  0
 
 ==============================================================================*/
-static long evrPatternSimProc(sSubRecord *psub)
+static long evrPatternSim(subRecord *psub)
 { 
-  epicsTimeStamp sys_time;
+  evrQueuePattern_ts     evrPatternWF_s;
+  epicsTimeStamp         prev_time;
+  double                 delta_time;
 
 /*------------- parse input into sub outputs ----------------------------*/
 
-  /* set outputs to the modifiers */
-  psub->d = psub->m;
-  psub->e = psub->n;
-  psub->f = psub->o;
-  psub->g = psub->p;
-  psub->h = psub->q;
-  /* bunch charge */
-  psub->i = psub->u;
-  /* grab beamcode, yy, from simulators */
-  psub->j = psub->x;
-  psub->k = psub->w;
-  /* pulse id */
-  psub->l = psub->v;
-  /* form evr timestamp from system time and w pulseid   */
-  if (epicsTimeGetCurrent (&sys_time)) {
-	psub->l = 0.0;
-	psub->val = EVR_INVALID;		
-	DEBUGPRINT(DP_ERROR,evrSubFlag,("evrPatternSimProc exiting -1! \n"))
-	return -1;
-  }
-  psub->r = (double) sys_time.secPastEpoch;
-  sys_time.nsec &= UPPER_15_BIT_MASK;
-  sys_time.nsec |= (unsigned long)(psub->v);
-  psub->s = (double) sys_time.nsec;
+  if (epicsTimeGetCurrent(&evrPatternWF_s.time)) {
+    psub->val = EVR_INVALID;
+    return -1;
+  /* The time is for 3 pulses in the future - add 1/120sec */
+  } else {
+    epicsTimeAddSeconds(&evrPatternWF_s.time, 1/120);
+    /* Overlay the pulse ID into the lower 17 bits of the nsec field */
+    evrTimePutPulseID(&evrPatternWF_s.time, (unsigned int)psub->l);
 
-  /* write to evr timestamp table and error check */
-  if (evrTimePut (&sys_time, evrTimeOK)){
-	/* if error writing last timestamp table index - set record invalid; pulse id =0*/
-	psub->l = PULSEID_INVALID;
-	psub->val = EVR_INVALID;		
-	DEBUGPRINT(DP_ERROR,evrSubFlag,("evrPatternSimProc exiting from evrPatternPutTime! \n"));
-	return -1;
-  }
-  return 0;
- 
-}
+    /* The timestamp must ALWAYS be increasing.  Check if this time
+       is less than the previous time (due to rollover) and adjust up
+       slightly using bit 17 (the lower-most bit of the top 15 bits). */
+    if (evrTimeGet(&prev_time, evrTimeNext3) == epicsTimeOK) {
+      delta_time = epicsTimeDiffInSeconds(&evrPatternWF_s.time, &prev_time);
+      if (delta_time < 0.0) {
+        epicsTimeAddSeconds(&evrPatternWF_s.time, PULSEID_BIT17/NSEC_PER_SEC);
+        evrTimePutPulseID(&evrPatternWF_s.time, (unsigned int)psub->l);
+      }
+    }
+    /* Timestamp done - now fill in the rest of the pattern */
+    evrPatternWF_s.header_s.type    = EVR_QUEUE_PATTERN;
+    evrPatternWF_s.header_s.version = EVR_QUEUE_PATTERN_VERSION;
+    evrPatternWF_s.modifier_a[0]    = ((epicsUInt32)psub->d) & 0xFFFFE000;
+    evrPatternWF_s.modifier_a[0]   |= ((((epicsUInt32)psub->j) << 8) & 0x1F00);
+    evrPatternWF_s.modifier_a[0]   |= ( ((epicsUInt32)psub->k) & YY_BIT_MASK);
+    evrPatternWF_s.modifier_a[1]    = psub->e;
+    evrPatternWF_s.modifier_a[2]    = psub->f;
+    evrPatternWF_s.modifier_a[3]    = psub->g;
+    evrPatternWF_s.modifier5        = psub->h;
+    evrPatternWF_s.bunchcharge      = psub->i;
 
-/*=============================================================================
-
-  Name: evrPatternSimMod5
-
-  Abs:  Sim Processing, Create Simulated Modifier5 from 20 simulated modmatch inputs
-
-		
-  Args: Type	            Name        Access	   Description
-        ------------------- -----------	---------- ----------------------------
-        subRecord *         psub        read       point to subroutine record
-
-  Rem:  Subroutine for EVRSIM:X:1:MODIFIER5
-
-  Side:
-
-  Sub Inputs/ Outputs:
-   Inputs:
-    A-U - MODMATCH1-F1
-    
-   Outputs:   
-    X  - lower 16 bits
-    Y  - upper 16 bits
-	VAL = MODIFIER5
-  Ret:  0
-
-==============================================================================*/
-static long evrPatternSimMod5 (sSubRecord *psub)
-{ 
-  unsigned long mod5 = 0;
-
-  mod5 |= (unsigned long)psub->a;
-  mod5 |= ( ((unsigned long)psub->b) << 1 );
-  mod5 |= ( ((unsigned long)psub->c) << 2 );
-  mod5 |= ( ((unsigned long)psub->d) << 3 );
-  mod5 |= ( ((unsigned long)psub->e) << 4 );
-  mod5 |= ( ((unsigned long)psub->f) << 5 );
-  mod5 |= ( ((unsigned long)psub->g) << 6 );
-  mod5 |= ( ((unsigned long)psub->h) << 7 );
-  mod5 |= ( ((unsigned long)psub->i) << 8 );
-  mod5 |= ( ((unsigned long)psub->j) << 9 );
-  mod5 |= ( ((unsigned long)psub->k) << 10 );
-  mod5 |= ( ((unsigned long)psub->l) << 11 );
-  mod5 |= ( ((unsigned long)psub->m) << 12 );
-  mod5 |= ( ((unsigned long)psub->n) << 13 );
-  mod5 |= ( ((unsigned long)psub->o) << 14 );
-  mod5 |= ( ((unsigned long)psub->p) << 15 );
-  mod5 |= ( ((unsigned long)psub->q) << 16 );
-  mod5 |= ( ((unsigned long)psub->r) << 17 );
-  mod5 |= ( ((unsigned long)psub->s) << 18 );
-  mod5 |= ( ((unsigned long)psub->u) << 19 );
-
-  psub->val = (double)mod5;
-  /* split into 2 16 bit words */
-  psub->x = (double) (mod5 & 0xFFFF);
-  psub->y = (double) (mod5 >>16 );
+    /* Send the pattern to the EVR pattern queue */
+    evrSend(0, sizeof(evrQueuePattern_ts)/sizeof(epicsUInt32), &evrPatternWF_s);  }
   return 0;
  
 }
 epicsRegisterFunction(evrPatternInit);
+epicsRegisterFunction(evrPatternProcInit);
 epicsRegisterFunction(evrPatternProc);
-epicsRegisterFunction(evrPatternSimInit);
-epicsRegisterFunction(evrPatternSimProc);
-epicsRegisterFunction(evrPatternSimMod5);
+epicsRegisterFunction(evrPatternState);
+epicsRegisterFunction(evrPatternSim);
