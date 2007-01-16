99968d3f3cd5a2a3ee8078da7689bec29b05ad12
Stephanie Allison <saa@slac.stanford.edu>
2007-01-16 20:55:21 +0000

    Remove non-time related macros from evrTime.h and added to evrPattern.h. Changed time table statuses to use epicsTime status defines instead of evrTimeStatus_te.

--- evrSupport/src/evrTime.h
+++ evrSupport/src/evrTime.h
@@ -37,28 +37,8 @@ extern "C" {
 #define PULSEID_BIT17           (0x00020000)    /* Bit up from pulse ID*/
 #define PULSEID_INVALID         (0x0001FFFF)    /* Invalid Pulse ID    */
 #define PULSEID_MAX             (0x0001FFDF)    /* Rollover value      */
-#define PULSEID_MAX_MINUS_2     (0x0001FFDD)    /* Rollover value - 2  */
 #define PULSEID_RESYNC          (0x0001E000)    /* Resync Pulse ID     */
 #define NSEC_PER_SEC            1E9             /* # nsec in one sec   */
-
-/* Masks used to decode beam code and YY from modifier1 */
-#define BEAMCODE_BIT_MASK       (0x0000001F)  /* Beam code mask        */
-                                              /* Left shift 8 first    */
-#define YY_BIT_MASK             (0x000000FF)  /* YY code mask          */
-
-/* Mask used to decode timeslot 1 to 6 from modifier2   */
-#define TIMESLOT_MASK           (0x0000003F)  /* timeslot   mask       */
-#define TIMESLOT1_MASK          (0x00000001)  /* timeslot 1 mask       */
-#define TIMESLOT2_MASK          (0x00000002)  /* timeslot 2 mask       */
-#define TIMESLOT3_MASK          (0x00000004)  /* timeslot 3 mask       */
-#define TIMESLOT4_MASK          (0x00000008)  /* timeslot 4 mask       */
-#define TIMESLOT5_MASK          (0x00000010)  /* timeslot 5 mask       */
-#define TIMESLOT6_MASK          (0x00000020)  /* timeslot 6 mask       */
-
-/* For status */
-typedef enum {
-  evrTimeOK, evrTimeLastGood, evrTimeInvalid
-} evrTimeStatus_te;
 
 /* For time ID */
 typedef enum {
@@ -67,7 +47,7 @@ typedef enum {
 #define  MAX_EVR_TIME  4
 
 int evrTimeGet       (epicsTimeStamp  *epicsTime_ps, evrTimeId_te id);
-int evrTimePut       (epicsTimeStamp  *epicsTime_ps, evrTimeStatus_te status);
+int evrTimePut       (epicsTimeStamp  *epicsTime_ps, int status);
 int evrTimePutPulseID(epicsTimeStamp  *epicsTime_ps, unsigned int pulseID);
 
 #ifdef __cplusplus
