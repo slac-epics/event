08735b79d267b920ef378fb9b7e046a95efe0199
Stephanie Allison <saa@slac.stanford.edu>
2007-01-18 16:54:06 +0000

    Add evrPattern for the pipeline PATTERN records. Correct bugs in evrModifier5Bits.

--- evrSupport/src/evrModifier5.c
+++ evrSupport/src/evrModifier5.c
@@ -1,8 +1,9 @@
 /*=============================================================================
  
-  Name: evrModifier5.c
+  Name: evrModifier5.c - Pattern Routines shared between EVR and EVG 
            evrModifer5     - Modifier 5 Creation using EDEF Check Bits
            evrModifer5Bits - Get EDEF Check Bits out of Modifier 5
+           evrPattern      - Pattern N,N-1,N-2,N-3
 
   Abs: This file contains all subroutine support for evr Pattern processing
        records.
@@ -37,12 +38,13 @@
 
 /* c includes */
 
-#include "sSubRecord.h"       /* for struct ssubRecord     */
+#include "subRecord.h"        /* for struct subRecord      */
+#include "sSubRecord.h"       /* for struct sSubRecord     */
 #include "registryFunction.h" /* for epicsExport           */
 #include "epicsExport.h"      /* for epicsRegisterFunction */
 
 #define EDEF_MAX    20
-#define NOEDEF_MASK 0xFFF0000
+#define NOEDEF_MASK 0xFFF00000
 
 /*=============================================================================
 
@@ -117,10 +119,53 @@ static long evrModifier5Bits(sSubRecord *psub)
 
   psub->u = mod5 & NOEDEF_MASK;
   for (edefIdx = 0; edefIdx < EDEF_MAX; edefIdx++, check_p++) {
-    *check_p = mod5 & (1 << edefIdx);
+    if (mod5 & (1 << edefIdx)) *check_p = 1.0;
+    else                       *check_p = 0.0;
   }
   psub->val = psub->v;
   return 0;
 }
+
+/*=============================================================================
+
+  Name: evrPattern
+
+  Abs:  360Hz Processing - make pattern data available in A through L.  Set
+        VAL to pattern status.
+		
+  Args: Type	            Name        Access	   Description
+        ------------------- -----------	---------- ----------------------------
+        subRecord *         psub        read       point to subroutine record
+
+  Rem:  Subroutine for IOC:LOCA:UNIT:PATTERN, PATTERNN-1, PATTERNN-2
+
+  Side: None
+
+  Sub Inputs/ Outputs:
+   Inputs:
+    A - PATTERN<N-1,N-2,N-3>.VAL (0 = OK, non-zero = Error)
+    B - Spare
+    C - Spare
+    D - MODIFIER1<N-1,N-2,N-3>
+    E - MODIFIER2<N-1,N-2,N-3>
+    F - MODIFIER3<N-1,N-2,N-3>
+    G - MODIFIER4<N-1,N-2,N-3>
+    H - MODIFIER5<N-1,N-2,N-3>
+    I - BUNCHARGE<N-1,N-2,N-3>
+    J - BEAMCODE<N-1,N-2,N-3>
+    K - Spare
+    L - PULSEID<N-1,N-2,N-3>
+   Outputs:
+    VAL = A (PATTERN<N-1,N-2,N-3>.VAL)
+  Ret:  -1 if VAL is not OK, 0 if VAL is OK.
+
+==============================================================================*/
+static long evrPattern(subRecord *psub)
+{
+  psub->val = psub->a;
+  if (psub->val) return -1;
+  return 0;
+}
 epicsRegisterFunction(evrModifier5);
 epicsRegisterFunction(evrModifier5Bits);
+epicsRegisterFunction(evrPattern);
