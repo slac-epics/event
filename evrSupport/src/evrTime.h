8edb4fcb97686b609cbf772b32f0da44958b25ff
Stephanie Allison <saa@slac.stanford.edu>
2009-04-01 00:15:03 +0000

    Make evrInitialize global.  Correct spelling of evrInitialize. Move fiducial routine initialization to evrInitialize (where it'll be done before tasks are started).  Thanks to Till for finding this.

--- evrSupport/src/evrTime.h
+++ evrSupport/src/evrTime.h
@@ -68,6 +68,7 @@ typedef unsigned long evrModifier_ta[MAX_EVR_MODIFIER];
 #define EVENT_MODULO36_MAX      236      /* Max modulo 36 event code    */
 #define MODULO36_MAX            36       /* # modulo 36 event codes     */
 
+int evrInitialize         (void);
 int evrTimeRegister(REGISTRYFUNCTION fiducialFuncIn);
 int evrTimeGetFromPipeline(epicsTimeStamp  *epicsTime_ps,
                            evrTimeId_te     id,
