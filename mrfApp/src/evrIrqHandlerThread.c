e062497ab79c1e3772c189f828c6208342793eef
Kukhee Kim <khkim@slac.stanford.edu>
2012-07-11 22:34:58 +0000

    add Irq handler thread for linux/linuxRT platform

---
+++ mrfApp/src/evrIrqHandlerThread.c
@@ -0,0 +1,27 @@
+#include <signal.h>
+#include <epicsThread.h>
+
+static int evrIrqHandlerThread(void *handler)
+{
+    void (*irqHandler)(int) = handler;
+    int signal;
+    sigset_t  sigSet;
+
+    sigemptyset(&sigSet);      
+    sigaddset(&sigSet,SIGIO);  
+
+    while(1) {
+        sigwait(&sigSet, &signal); 
+        if(irqHandler) irqHandler(signal);
+    }
+
+    return 0;
+}
+
+void EvrIrqHandlerThreadCreate(void (*handler) (int))
+{
+
+    epicsThreadMustCreate("evrIrqHandler", epicsThreadPriorityHigh+5,
+                          epicsThreadGetStackSize(epicsThreadStackMedium),
+                          (EPICSTHREADFUNC)evrIrqHandlerThread,handler);
+}
