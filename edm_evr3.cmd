b1789fe980490b11947c2b7e32dfbb88283dbfde
Bruce Hill <bhill@slac.stanford.edu>
2013-09-28 08:43:48 +0000

    Reworked the LCLS and PCDS event code screens using embedded windows. Uses MCC PV's for most current event code descriptions and delays.   Added delay values to pcds event code allocation screen.
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/trunk/pcds/epics/modules/event/current@14070 ececa60f-9f63-0410-97c7-ea05888eb260

--- edm_evr3.cmd
+++ edm_evr3.cmd
@@ -3,6 +3,6 @@
 # Setup edm environment
 source /reg/g/pcds/setup/epicsenv-3.14.12.sh
 
-edm -x -eolc -m "IOC=LAS:R52:IOC:30,EVR=LAS:R52:EVR:30,DEC=14" evrScreens/evr.edl evrScreens/lclsEventCodes.edl evrScreens/emb-event-code.edl evrScreens/emb-10-ec.edl evrScreens/emb-7-ec.edl &
+edm -x -eolc -m "IOC=LAS:R52:IOC:30,EVR=LAS:R52:EVR:30,DEC=9,EVG=EVNT:SYS0,ECS=ECS:SYS0" evrScreens/evr.edl evrScreens/emb-evg-*-ec.edl evrScreens/emb-ecs-*-ec.edl &
 #edm -x -eolc -m "IOC=UND:R02:IOC:16,EVR=UND:R02:EVR:16" evrScreens/evr.edl evrScreens/lclsEventCodes.edl evrScreens/emb-event-code.edl &
 
