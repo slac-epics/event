67e337d90d50d60c9696e269b3a7eb45d1c856dd
Bruce Hill <bhill@slac.stanford.edu>
2013-10-02 22:07:39 +0000

    Reworked the substitution files to fully populate the required records for both LCLS and Event Sequencer event codes. The module now builds under $TOP/db 3 sample db files that can be directly installed in your ioc $TOP/db and loaded w/ just EVR and CARD macros if all you need is a basic evrPmc230.db, evrVme230.db, or evrSLAC.db
    Event sequencer allocation screen moved here from ioc/common/eventSeqCfg
    so all EVR's have access to it from the regular EVR screens.
    LCLS and Event sequencer screens updated to show rates, delays,
    and names for all the event codes.  Allocation screen now includes
    hutch color coded backgrounds to show more clearly which event codes
    are used by which hutches.
    
    Added new stringin and stringout record types for dev MRF Event Receiver
    that support saving and recalling event code names by number.
    
    Used new records to rework how the event descriptions are updated
    on the Event Trigger screens.  Now users can update the sequencer event
    code names on any EVR and the names will update on all EVR screens
    running the new version.
    
    
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/trunk/pcds/epics/modules/event/current@14121 ececa60f-9f63-0410-97c7-ea05888eb260

--- edm_evr3.cmd
+++ edm_evr3.cmd
@@ -3,6 +3,15 @@
 # Setup edm environment
 source /reg/g/pcds/setup/epicsenv-3.14.12.sh
 
-edm -x -eolc -m "IOC=LAS:R52:IOC:30,EVR=LAS:R52:EVR:30,DEC=9,EVG=EVNT:SYS0,ECS=ECS:SYS0" evrScreens/evr.edl evrScreens/emb-evg-*-ec.edl evrScreens/emb-ecs-*-ec.edl &
-#edm -x -eolc -m "IOC=UND:R02:IOC:16,EVR=UND:R02:EVR:16" evrScreens/evr.edl evrScreens/lclsEventCodes.edl evrScreens/emb-event-code.edl &
+edm -x -eolc	\
+-m "IOC=LAS:R52:IOC:30"	\
+-m "EVR=LAS:R52:EVR:30"	\
+-m "DEC=9"	\
+-m "N1=87,N2=171,N3=201"	\
+-m "EVG=EVNT:SYS0"	\
+-m "ECS=ECS:SYS0"	\
+evrScreens/evr.edl	\
+evrScreens/emb-evg-*-ec.edl	\
+evrScreens/emb-ecs-*-ec.edl	\
+&
 
