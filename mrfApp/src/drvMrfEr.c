1268d64290073cd4a0d6a87e1c0f0fb5cae2d15c
Bruce Hill <bhill@slac.stanford.edu>
2013-12-03 19:33:17 +0000

    Added support for new EVR firmware
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/trunk/pcds/epics/modules/event/current@14959 ececa60f-9f63-0410-97c7-ea05888eb260

--- mrfApp/src/drvMrfEr.c
+++ mrfApp/src/drvMrfEr.c
@@ -1244,6 +1244,8 @@ int ErConfigure (
 	case (PMC_EVR_FIRMWARE_REV_LINUX1 & 0xFFFF):	/* VME Firmware field is only 16 bits */
 	case (PMC_EVR_FIRMWARE_REV_LINUX2 & 0xFFFF):	/* VME Firmware field is only 16 bits */
 	case (PMC_EVR_FIRMWARE_REV_LINUX3 & 0xFFFF):	/* VME Firmware field is only 16 bits */
+	case (PMC_EVR_FIRMWARE_REV_LINUX4 & 0xFFFF):	/* VME Firmware field is only 16 bits */
+	case (PMC_EVR_FIRMWARE_REV_LINUX5 & 0xFFFF):	/* VME Firmware field is only 16 bits */
 	    fprintf ( stderr,
 	       "\nErConfigure ERROR: This PMC EVR has firmware for a linux\n"
 	       "based system and cannot be used under RTEMS!\n" );
