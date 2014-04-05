3d90b7406f3d9ff2596d5609af7f83543944d206
Bruce Hill <bhill@slac.stanford.edu>
2014-04-05 02:40:11 +0000

    Adding a set of python scripts that are useful for saving a copy of the current EVR settings, comparing previously saved settings to the current EVR values, and restoring previously saved settings.
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/trunk/pcds/epics/modules/event/current@16414 ececa60f-9f63-0410-97c7-ea05888eb260

---
+++ cmpConfig.py
@@ -0,0 +1,69 @@
+#! /usr/bin/env python
+# pyca script for python 2.7.2
+# Before running;
+# source /reg/g/pcds/setup/epicsenv-3.14.12.sh
+# source /reg/g/pcds/setup/python27.sh
+# pythonpathmunge /reg/g/pcds/pds/pyca
+#
+
+from Pv import Pv
+import pyca
+import sys
+
+from options import Options
+
+def stringToScalar( value ):
+    try:
+        value = int(value)
+    except:
+        value = float(value)
+    return value
+
+def cmpConfigFile( fileName ):
+    try:
+        with open( fileName ) as f:
+            for line in f:
+                valStr = ''
+                if len(line.split()) == 1:
+                    [ pvName ] = line.split()
+                elif len(line.split()) == 2:
+                    [ pvName, valStr ] = line.split()
+                else:
+                    [ pvName, valStr, crlfStr ] = line.split('"')
+                if valStr == '""':
+                    valStr = ''
+
+                pv = Pv( pvName )
+                pv.connect(0.1)
+                pv.get( False, 0.1 )
+                if isinstance( pv.value, str ):
+                    cfgValue = valStr
+                else:
+                    cfgValue = stringToScalar( valStr )
+                if cfgValue == pv.value:
+                    continue
+                if	(	isinstance( pv.value, float ) and
+                        (pv.value - cfgValue)/cfgValue < 0.001 ):
+                    continue
+                print "actual %s %-.30s, cfg %-.30s" % ( pv.name, pv.value, cfgValue )
+
+    except pyca.pyexc, msg:
+        print "failed: pyca exception: ", msg
+    except pyca.caexc, msg:
+        print "failed: channel access exception: ", msg
+    except Exception, msg:
+        print "failed:", msg.__class__.__name__, "exception: ", msg
+
+# ----------------------------------------------------------------------
+if __name__ == "__main__":
+
+    options = Options( ['configFile'], [], [] )
+    try:
+        options.parse()
+    except Exception, msg:
+        options.usage( str(msg) )
+        sys.exit()
+
+    cmpConfigFile( options.configFile )
+    pyca.flush_io()
+    # Done
---
+++ saveEvrSettings.py
@@ -0,0 +1,159 @@
+#! /usr/bin/env python
+# pyca script for python 2.7.2
+# Before running;
+# source /reg/g/pcds/setup/epicsenv-3.14.12.sh
+# source /reg/g/pcds/setup/python27.sh
+# pythonpathmunge /reg/g/pcds/pds/pyca
+#
+
+from Pv import Pv
+import pyca
+import sys
+
+from options import Options
+
+def printPvNameValue( pvName ):
+    try:
+        pv = Pv( pvName )
+        pv.connect(0.1)
+        pv.get(False, 0.1)
+        if isinstance( pv.value, str ):
+            print "%s \"%-.30s\"" % ( pv.name, pv.value )
+        else:
+            print "%s %-.30s" % ( pv.name, pv.value )
+    except pyca.pyexc, msg:
+        print >> sys.stderr, "failed: pyca exception: ", msg
+    except pyca.caexc, msg:
+        print >> sys.stderr, "failed: channel access exception: ", msg
+    except Exception, msg:
+        print >> sys.stderr, "failed:", msg.__class__.__name__, "exception: ", msg
+
+def showPulseGenSettings( evrName, dgNumber ):
+    try:
+        # Print the Delay Generator enable value
+        printPvNameValue( evrName + ":CTRL.DG%1dE" % ( dgNumber ) )
+
+        # Print Polarity
+        printPvNameValue( evrName + ":CTRL.DG%1dP" % ( dgNumber ) )
+
+        #
+        # Pre-scale, delay, and width need special handling to
+        # convert pre-scalar to 1
+        #
+
+        # Get the Pre-scale value for this delay generator
+        preScalePv = Pv( evrName + ":CTRL.DG%1dC" % ( dgNumber ) )
+        preScalePv.connect(0.1)
+        preScalePv.get(False, 0.1)
+        # Note we force preScale to 1
+        print "%s %-.30s" % ( preScalePv.name, 1 )
+
+        # Delay
+        delayPv = Pv( evrName + ":CTRL.DG%1dD" % ( dgNumber ) )
+        delayPv.connect(0.1)
+        delayPv.get(False, 0.1)
+        print "%s %-.30s" % ( delayPv.name, delayPv.value * preScalePv.value )
+
+        # Width
+        widthPv = Pv( evrName + ":CTRL.DG%1dW" % ( dgNumber ) )
+        widthPv.connect(0.1)
+        widthPv.get(False, 0.1)
+        print "%s %-.30s" % ( widthPv.name, widthPv.value * preScalePv.value )
+
+    except pyca.pyexc, msg:
+        print >> sys.stderr, "failed: pyca exception: ", msg
+    except pyca.caexc, msg:
+        print >> sys.stderr, "failed: channel access exception: ", msg
+    except Exception, msg:
+        print >> sys.stderr, "failed:", msg.__class__.__name__, "exception: ", msg
+
+
+def saveTriggerSettings( evrName, trigNumber ):
+    triggerName = evrName + ":TRIG%1d:" % ( trigNumber )
+    printPvNameValue( triggerName + "TEC.VAL" )
+    printPvNameValue( triggerName + "TCTL.VAL" )
+    printPvNameValue( triggerName + "TCTL.DESC" )
+    printPvNameValue( triggerName + "TDES.VAL" )
+    printPvNameValue( triggerName + "TDES.DESC" )
+    printPvNameValue( triggerName + "TPOL.VAL" )
+    printPvNameValue( triggerName + "TPOL.DESC" )
+    printPvNameValue( triggerName + "TWID.VAL" )
+    printPvNameValue( triggerName + "TWID.DESC" )
+
+def saveEventCtrlSettings( evrName, ctrlNumber ):
+    eventCtrlName = evrName + ":EVENT%1dCTRL" % ( ctrlNumber )
+    printPvNameValue( eventCtrlName + ".ENM" )
+    printPvNameValue( eventCtrlName + ".OUT0" )
+    printPvNameValue( eventCtrlName + ".OUT1" )
+    printPvNameValue( eventCtrlName + ".OUT2" )
+    printPvNameValue( eventCtrlName + ".OUT3" )
+    printPvNameValue( eventCtrlName + ".OUT4" )
+    printPvNameValue( eventCtrlName + ".OUT5" )
+    printPvNameValue( eventCtrlName + ".OUT6" )
+    printPvNameValue( eventCtrlName + ".OUT7" )
+    printPvNameValue( eventCtrlName + ".VME" )
+
+# ----------------------------------------------------------------------
+if __name__ == "__main__":
+
+    options = Options( ['evrPvName'], [], [] )
+    try:
+        options.parse()
+    except Exception, msg:
+        options.usage( str(msg) )
+        sys.exit()
+
+    evrPvName = options.evrPvName
+    try:
+        statusPv = Pv( evrPvName + ":STATUS" )
+        statusPv.connect(0.1)
+        statusPv.get(False, 0.1)
+    except Exception, msg:
+        print "EVR not accessible: ", msg
+        sys.exit()
+
+    saveTriggerSettings( evrPvName, 0 )
+    saveTriggerSettings( evrPvName, 1 )
+    saveTriggerSettings( evrPvName, 2 )
+    saveTriggerSettings( evrPvName, 3 )
+    saveTriggerSettings( evrPvName, 4 )
+    saveTriggerSettings( evrPvName, 5 )
+    saveTriggerSettings( evrPvName, 6 )
+    saveTriggerSettings( evrPvName, 7 )
+    saveTriggerSettings( evrPvName, 8 )
+    saveTriggerSettings( evrPvName, 9 )
+    saveTriggerSettings( evrPvName, 10 )
+    saveTriggerSettings( evrPvName, 11 )
+
+    showPulseGenSettings( evrPvName, 0 )
+    showPulseGenSettings( evrPvName, 1 )
+    showPulseGenSettings( evrPvName, 2 )
+    showPulseGenSettings( evrPvName, 3 )
+    showPulseGenSettings( evrPvName, 4 )
+    showPulseGenSettings( evrPvName, 5 )
+    showPulseGenSettings( evrPvName, 6 )
+    showPulseGenSettings( evrPvName, 7 )
+
+    saveEventCtrlSettings( evrPvName, 1 )
+    saveEventCtrlSettings( evrPvName, 2 )
+    saveEventCtrlSettings( evrPvName, 3 )
+    saveEventCtrlSettings( evrPvName, 4 )
+    saveEventCtrlSettings( evrPvName, 5 )
+    saveEventCtrlSettings( evrPvName, 6 )
+    saveEventCtrlSettings( evrPvName, 7 )
+    saveEventCtrlSettings( evrPvName, 8 )
+    saveEventCtrlSettings( evrPvName, 9 )
+    saveEventCtrlSettings( evrPvName, 10 )
+    saveEventCtrlSettings( evrPvName, 11 )
+    saveEventCtrlSettings( evrPvName, 12 )
+    saveEventCtrlSettings( evrPvName, 13 )
+    saveEventCtrlSettings( evrPvName, 14 )
+
+    printPvNameValue( evrPvName + ":FP0L" )
+    printPvNameValue( evrPvName + ":FP0L.DESC" )
+    printPvNameValue( evrPvName + ":FP1L" )
+    printPvNameValue( evrPvName + ":FP1L.DESC" )
+    printPvNameValue( evrPvName + ":FP2L" )
+    printPvNameValue( evrPvName + ":FP2L.DESC" )
+
+    # Done
